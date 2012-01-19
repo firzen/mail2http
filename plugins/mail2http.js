9660bc90822b25387969a400005b4096db08e942
newdongyuwei <newdongyuwei@gmail.com>
2012-01-19 09:30:24 +0000

    post mail to http use multipart/form-data format

--- plugins/mail2http.js
+++ plugins/mail2http.js
@@ -1,59 +1,75 @@
-var console = require('console'),
-    request = require('request'),
-    fs = require('fs'),
-    MailParser = require("mailparser").MailParser, 
-    mailparser = new MailParser({
-        //streamAttachments : true
-    });
+var console     = require('console'),
+    fs          = require("fs"),
+    http        = require("http"),
+    multiparter = require("multiparter"),
+    MailParser  = require("mailparser").MailParser;
+    
 
 var postURL;
 exports.register = function () {
     postURL = this.config.get('mail2http.url');
 };
-
-/*
-attachment event only fired once??!!
-mailparser.on("attachment", function(attachment){
-    attachment.stream.pipe(fs.createWriteStream(process.cwd() + "/attachments/" + attachment.fileName ));
-});
-*/
-
-var rawMailData;
-function post(mail){
-    var message = {
-        uri: postURL, 
-        method: "POST", 
-        headers: {'content-type':'application/json', 'accept':'application/json'},
-        body: JSON.stringify({
-            rawMailData:rawMailData,//这里先把邮件原始内容以文本形式post过去.后续再直接把attachments以multipart/form-data文件流格式直接post过去
-            to:mail.to,
-            from:mail.from,
-            subject:mail.subject,
-            html:mail.html,
-            text:mail.text
-        })
-    };
-    request(message, function(err, resp, body) {
-      console.log(err, resp, body);
-    });
-    
-    rawMailData = null;
-}
-
-mailparser.on("end", function(mail){ 
-    console.log("mail:", mail); 
-    mail.attachments.forEach(function(attachment,i){
-        fs.writeFile(process.cwd() + "/attachments/" + attachment.generatedFileName, attachment.content, function (err) {
-          console.log( attachment.generatedFileName + ' is saved!');
-        });
-    });
-    
-    post(mail);
-});
-
 exports.hook_data_post = function (next, connection) {
     next();
-    rawMailData = connection.transaction.data_lines.join('');
-    mailparser.write(rawMailData);
-    mailparser.end();
+    parseMail(connection.transaction.data_lines.join(''));
 };
+
+function parseMail(data){
+    var mailparser  = new MailParser({
+        streamAttachments : true
+    });
+
+    mailparser.on("attachment", function(attachment){
+        attachment.stream.pipe(fs.createWriteStream(process.cwd() + "/attachments/" + attachment.fileName ));
+    });
+
+    mailparser.on("end", function(mail){ 
+        postMail2HTTP(mail);
+    });
+
+    mailparser.write(data);
+    mailparser.end();
+    mailparser = null;
+}
+
+function postMail2HTTP(oMail){
+    var host = postURL.split('http://')[1].split(':')[0], port = postURL.split('http://')[1].split(':')[1]
+    var request = new multiparter.request(http, {
+        host: host,
+        port: port,
+        path: "/upload",
+        method: "POST"
+    });
+
+    var attachments = oMail.attachments;
+    delete oMail.headers;
+    delete oMail.attachments;
+
+
+    for(var k in oMail){
+        request.setParam(k, typeof oMail[k] === 'string' ? oMail[k] : JSON.stringify(oMail[k]));
+    }
+
+    var files = [];
+    attachments.forEach(function(attachment,i){
+        var path = process.cwd() + "/attachments/" + attachment.fileName;
+        request.addStream(
+            "attachment" + i,
+            attachment.fileName,
+            attachment.contentType,
+            attachment.length,
+            fs.createReadStream(path)
+        );
+        files.push(path);
+    });
+
+    request.send(function(error, response) {
+        if(response.statusCode === 200){
+            files.forEach(function(path,i){
+                fs.unlink(path, function (err) {
+                    console.log('successfully deleted ',path);
+                });
+            });
+        }
+    });
+}
