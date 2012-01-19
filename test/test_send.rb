6340f058d0b70cfd47c909fe16a67c0f53bba6f4
newdongyuwei <newdongyuwei@localhost>
2012-01-19 17:22:13 +0800

    post mail to http

---
+++ test/test_send.rb
@@ -0,0 +1,31 @@
+require 'rubygems'
+require 'mail'
+
+hostname = 'localhost'#`hostname`.strip
+          
+Mail.defaults do
+  delivery_method :smtp, { :address => hostname,
+    :port    => 25,
+    :domain  => hostname
+  }
+end
+
+mail = Mail.new do
+	from    'from@localhost'
+	to      'to@localhost'
+	subject 'test email subject'
+
+	text_part do
+		body 'This is plain text'
+	end
+
+	html_part do
+		content_type 'text/html; charset=UTF-8'
+		body '<h1>This is HTML</h1>'
+	end
+
+	add_file File.join(File.dirname(__FILE__),'attachment.txt')
+	add_file File.join(File.dirname(__FILE__),'face.jpeg')
+end
+
+mail.deliver!
