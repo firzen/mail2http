c20d24758d159ad3307dbbbfb0e803bdc81f5b3b
newdongyuwei <newdongyuwei@localhost>
2012-02-02 17:20:55 +0800

    refork worker

--- plugins/mail2weibo.js
+++ plugins/mail2weibo.js
@@ -4,8 +4,18 @@
     
     var worker = cp.fork(__dirname + '/weibo_worker.js');
 
-    worker.on('message', function(m) {
-      console.log('message from worker:', m);
+    worker.on('exit', function () {
+        var masterIsAlive = true;
+        try{
+            process.cwd();
+        }catch(e){
+            masterIsAlive = false;
+            console.error(e);
+        }
+        if(masterIsAlive){//如果主进程还活着,就重新fork worker
+            console.log('worker is about to exit, refork the work.');
+            worker = cp.fork(__dirname + '/weibo_worker.js');
+        }
     });
 
     var to ;
